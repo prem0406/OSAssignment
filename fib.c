a35ba15a1c0b6cb30219770911d1e3cad60342eb
prem0406 <premk191@gmail.com>
2018-04-12 10:14:57 +0530

    modified

--- fib.c
+++ fib.c
@@ -3,7 +3,7 @@
 #include<pthread.h>
 #include<unistd.h>
 
-
+extern int arr*;
 
 void *fib(void *p);
 int main()
@@ -13,7 +13,7 @@ int main()
 	printf("\nEnter a no: ");
 	int n;
 	scanf("%d",&n);
-	//arr=(int*)malloc(n*sizeof(int));
+	arr=(int*)malloc(n*sizeof(int));
 	pthread_create(&thread,NULL,fib,(void*)(intptr_t)n);
 	pthread_join(thread,NULL);
 	printf("\nFibbonacci Series: ");
@@ -30,14 +30,17 @@ void *fib(void *p)
 	int i=0;
 	int j=1;
 	int temp;
-	printf("\nThis is Fib: ");
-	printf("%d  ",i);
-	printf("%d  ",j);
-	for(int k=0;k<n-2;k++)
+	//printf("\nThis is Fib: ");
+	//printf("%d  ",i);
+	//printf("%d  ",j);
+	arr=&i;
+	arr+1=&j;
+	for(int k=2;k<n;k++)
 	{
 		temp=j+i;
 		i=j;
 		j=temp;
-		printf("%d  ",temp);
+		arr+k=&temp;
+		//printf("%d  ",temp);
 	}
 }
